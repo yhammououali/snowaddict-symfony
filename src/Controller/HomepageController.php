24639814ad14bc000bc79f43d30729f7614f075a
Yassin HAMMOU OUALI <y.hammououali@gmail.com>
2023-01-27 17:06:03 +0100

    adding new components

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\FigureRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomepageController extends AbstractController
 {
     #[Route('/homepage/{name}', name: 'app_homepage', methods: ['GET'])]
-    public function homepage(string $name): Response
+    public function homepage(string $name, FigureRepository $figureRepository): Response
     {
+
+
         return $this->render('homepage/index.html.twig', [
-            'firstName' => $name
+            'firstName' => $name,
+            'figures' => $figureRepository->findAll(),
         ]);
     }
 }
